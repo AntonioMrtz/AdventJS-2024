8d9eccfbcc6388fb2db7d350744d78d3e5e4db00
Antonio Martinez <antonio.martinez@bionet.com>
2024-12-20 13:06:43 +0100

    feat: add day 19

--- 07/07.ts
+++ 07/07.ts
@@ -25,14 +25,14 @@ console.log(fixPackages("a(cb)de"));
 // ➞ "abcde"
 // We reverse "cb" inside the parentheses
 
-// console.log(fixPackages("a(bc(def)g)h"));
-// // ➞ "agdefcbh"
-// // 1st we reverse "def" → "fed", then we reverse "bcfedg" → "gdefcb"
+console.log(fixPackages("a(bc(def)g)h"));
+// ➞ "agdefcbh"
+// 1st we reverse "def" → "fed", then we reverse "bcfedg" → "gdefcb"
 
-// console.log(fixPackages("abc(def(gh)i)jk"));
-// // ➞ "abcighfedjk"
-// // 1st we reverse "gh" → "hg", then "defhgi" → "ighfed"
+console.log(fixPackages("abc(def(gh)i)jk"));
+// ➞ "abcighfedjk"
+// 1st we reverse "gh" → "hg", then "defhgi" → "ighfed"
 
-// console.log(fixPackages("a(b(c))e"));
-// // ➞ "acbe"
-// // 1st we reverse "c" → "c", then "bc" → "cb"
+console.log(fixPackages("a(b(c))e"));
+// ➞ "acbe"
+// 1st we reverse "c" → "c", then "bc" → "cb"
